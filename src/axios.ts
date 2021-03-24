92bf3e8c956d252256f812608d6e54a343a083cb
wangqing <qing.wang@tom.com>
2021-03-24 21:27:12 +0800

    创建axios

---
+++ src/axios.ts
@@ -0,0 +1,38 @@
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types';
+import { transformData, transformRequest } from '../helpers/data';
+import { processHeaders } from '../helpers/headers';
+import { buildUrl } from '../helpers/url';
+import xhr from './xhr';
+
+function axios(config: AxiosRequestConfig): AxiosPromise {
+  processConfig(config);
+  return xhr(config).then(res => {
+    return transformResponseData(res);
+  });
+}
+
+function processConfig (config: AxiosRequestConfig):void {
+  config.url = transformUrl(config);
+  config.data = transformRequestData(config);
+  config.headers = transformHeaders(config);
+}
+
+function transformUrl (config: AxiosRequestConfig):string {
+  const { url, params } = config;
+  return buildUrl(url, params);
+}
+
+function transformRequestData (config: AxiosRequestConfig):any {
+  return transformRequest(config.data);
+}
+
+function transformHeaders (config: AxiosRequestConfig):any {
+  return processHeaders(config.headers, config.data);
+}
+
+function transformResponseData (res: AxiosResponse): AxiosResponse {
+  res.data = transformData(res.data);
+  return res;
+}
+
+export default axios;
